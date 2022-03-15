fe7ab53cd2ac2941dfbfa09838e4e933785acb80
Amaury Levé <amauryleve@microsoft.com>
2022-03-15 15:34:38 +0100

    Verify no marshalling behavior
    hh

---
+++ InterfacePartOfPluginPackage/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using Lib;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+
+class Proxy //: IA, IDisposable
+{
+    private readonly PluginLoadContext _context;
+    private readonly Assembly _assembly;
+
+    public Proxy(string dllPath)
+    {
+        _context = new PluginLoadContext(dllPath);
+        _assembly = _context.LoadFromAssemblyName(AssemblyName.GetAssemblyName(dllPath));
+    }
+
+    public Out MyMethod(In @in)
+    {
+        foreach (Type type in _assembly.GetTypes())
+        {
+            if (typeof(IA).IsAssignableFrom(type))
+            {
+                Console.WriteLine("Type is assignable to IA, creating an instance and calling MyMethod.");
+                var instance = Activator.CreateInstance(type) as IA;
+                return instance!.MyMethod(@in);
+            }
+        }
+
+        Console.WriteLine("Could not find any type assignable to IA.");
+        return null!;
+    }
+
+    public void Dispose()
+    {
+        _context.Unload();
+    }
+}
+
+internal class PluginLoadContext : AssemblyLoadContext
+{
+    private readonly AssemblyDependencyResolver _resolver;
+
+    public PluginLoadContext(string pluginPath) : base(isCollectible: true)
+    {
+        _resolver = new AssemblyDependencyResolver(pluginPath);
+    }
+
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        var assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+        if (assemblyPath != null)
+        {
+            return LoadFromAssemblyPath(assemblyPath);
+        }
+
+        return null;
+    }
+
+    protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+    {
+        var libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+        if (libraryPath != null)
+        {
+            return LoadUnmanagedDllFromPath(libraryPath);
+        }
+
+        return IntPtr.Zero;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var currentLocation = new DirectoryInfo(typeof(Program).Assembly.Location);
+        var rootDir = currentLocation.Parent!.Parent!.Parent!.Parent!.Parent;
+        // TODO: After the first build, copy Lib.dll to the root folder. After, you can run the
+        // app and see that the types are not compatible.
+        var x = new Proxy(Path.Combine(rootDir!.FullName, "Lib.dll"));
+        x.MyMethod(new In());
+        (x as IDisposable)?.Dispose();
+    }
+}
---
+++ InterfacePartOfPluginPackage/Lib/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Lib
+{
+    public interface IA
+    {
+        Out MyMethod(In @in);
+    }
+
+    public class In { }
+    public class Out { }
+
+    public class A : IA
+    {
+        public Out MyMethod(In @in)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
